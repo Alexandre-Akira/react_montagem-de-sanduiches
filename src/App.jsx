5fc81c6d66494a97d7d9abc17570ce9fc4e32370
Alexandre Akira Enjiu <akira.enjiu@gmail.com>
2022-03-10 17:56:02 -0300

    Input component created!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import AppHeader from "./Components/Molecules/AppHeader";
 import OrderOverview from "./Components/Organisms/OrderOverview";
+import Input from "./Components/Atoms/Input";
 
 function App() {
   return (
     <div className="App">
       <AppHeader title={"Pague Seu Sanduiche"} />
+      <Input className="input--form" />
     </div>
   );
 }
--- src/Components/Atoms/Input/index.jsx
+++ src/Components/Atoms/Input/index.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import "./style.css";
 
-function Input() {
-  return <input className="input" />;
+function Input({ className }) {
+  const [value, setValue] = useState("");
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+  return <input className={className} value={value} onChange={(event) => handleChange(event)} />;
 }
 
 export default Input;
